a87246176ca5a25708dcbd2d7f0e45432950cdeb
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-04-01 19:06:37 -0700

    Don't jitter scroll on execution if already in viewport
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- web/src/components/Runme/Console.tsx
+++ web/src/components/Runme/Console.tsx
@@ -345,10 +345,23 @@ function Console({
         el.appendChild(terminalEnd)
 
         setTimeout(() => {
-          terminalEnd.scrollIntoView({ behavior: 'smooth' })
+          if (!isInViewport(terminalEnd)) {
+            terminalEnd.scrollIntoView({ behavior: 'smooth' })
+          }
         }, 0)
       }}
     ></div>
+  )
+}
+
+function isInViewport(element: Element) {
+  const rect = element.getBoundingClientRect()
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   )
 }
 
