803655506aa1c0e003b1ef73f74705f51cab9eb4
zizheng <zizheng@openai.com>
2025-06-03 17:03:58 -0700

    feat(eval): update eval command to accept a single YAML file and process it into an ExperimentRun

--- app/cmd/eval.go
+++ app/cmd/eval.go
@@ -1,23 +1,46 @@
 package cmd
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/jlewi/cloud-assistant/app/pkg/ai"
 	"github.com/jlewi/cloud-assistant/app/pkg/application"
+	"github.com/jlewi/cloud-assistant/protos/gen/cassie"
 	"github.com/spf13/cobra"
+	"google.golang.org/protobuf/encoding/protojson"
+	"gopkg.in/yaml.v3"
 )
 
 func NewEvalCmd() *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "eval <yaml-file> <target-url>",
-		Short: "Run evaluation using a YAML file and target URL",
-		Args:  cobra.ExactArgs(2),
+		Use:   "eval <yaml-file>",
+		Short: "Run evaluation using a single experiment YAML file",
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			app := application.NewApp()
 			if err := app.LoadConfig(cmd); err != nil {
 				return err
 			}
-			app.Config.CloudAssistant.TargetURL = args[1]
-			_, err := ai.EvalFromYAML(args[0], app.Config.CloudAssistant)
+
+			// Read the experiment YAML file
+			data, err := os.ReadFile(args[0])
+			if err != nil {
+				return err
+			}
+			var m map[string]interface{}
+			if err := yaml.Unmarshal(data, &m); err != nil {
+				return err
+			}
+			jsonBytes, err := json.Marshal(m) // use json as intermediate format
+			if err != nil {
+				return err
+			}
+			var experiment cassie.ExperimentRun
+			if err := protojson.Unmarshal(jsonBytes, &experiment); err != nil {
+				return err
+			}
+			_, err = ai.EvalFromExperimentRun(&experiment)
 			if err != nil {
 				return err
 			}
